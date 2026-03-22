9809c73401768dbd78ce343bcd9f9e1a4c068a26
Deval Hole <devalhole.pict@gmail.com>
2026-03-22 23:23:12 +0530

    fix: correct import path for Login component in app.routes.jsx

--- frontend/src/app/app.routes.jsx
+++ frontend/src/app/app.routes.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router";
 import Landing from "../features/landing/pages/Landing";
-import Login from "../features/auth/pages/Login";
+import Login from "../features//auth/pages/login";
 import Register from "../features/auth/pages/Register";
 import Dashboard from "../features/chat/pages/Dashboard";
 import Protected from "../features/auth/components/Protected";
